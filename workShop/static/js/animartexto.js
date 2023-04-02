b902c483a4238658646191ea7c3a372d34162452
alejo355 <117774931+alejo355@users.noreply.github.com>
2023-04-02 17:24:15 -0500

    Actualizacion 0.5

---
+++ workShop/static/js/animartexto.js
@@ -0,0 +1,15 @@
+var tit = document.title;
+var c = 0;
+function writetitle() {
+    document.title = tit.substring(0, c);
+    if (c == tit.length) {
+        c = 0;
+        setTimeout("writetitle()", 1000)
+    }
+    else {
+        c++;
+        setTimeout("writetitle()", 200)
+    }
+}
+
+writetitle()
